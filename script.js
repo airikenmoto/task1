dc696d20461c8926b75d2ec07d61b6916467627e
airikenmoto <uglykids666@icloud.com>
2024-05-14 22:35:49 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let lists = document.getElementsByClassName("pull-down-list");
-console.log(lists);
+
+window.addEventListener('mouseover',function(){
+    const pullDownButton = document.getElementById("lists");
+})
+
+window.addEventListener('mouseout',function(){
+    const pullDownButton = document.getElementById("lists");
+})
