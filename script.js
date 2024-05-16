3a1060ae553b935db6a3d59395bedc7791cc3b54
airikenmoto <uglykids666@icloud.com>
2024-05-16 23:18:39 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let lists = document.getElementsByClassName("pull-down-list");
 
-window.addEventListener('mouseover',function(){
+window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener('mouseover',function(){
+        pullDownButton.setAttribute('style',"background-color:blue;")
+        console.log("乗せたときは青色")
+    })
 })
 
-window.addEventListener('mouseout',function(){
+window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener('mouseout',function(){
+        pullDownButton.removeAttribute('style',"background-color:red;")
+        console.log("外れた")
+    })
+})
+
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists");
+    pullDownButton.addEventListener('click',function(){
+        pullDownButton.setAttribute('style',"background-color:green;")
+        console.log("クリック")
+    })
 })
