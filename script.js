d760a5b394edebce411c75c3b1418f8f84a2eb6a
airikenmoto <uglykids666@icloud.com>
2024-05-19 22:14:50 +0900

    文字列の取得編

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
+function pullDown(){
 let lists = document.getElementsByClassName("pull-down-list");
-
-window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists");
+    const pullDownParents = document.getElementById("pull-down");
+    const pullDownChild = document.querySelectorAll(".pull-down-list");
+    const pullDownChildren = document.getElementById("list1");
+    const currentList = this.document.getElementById("current-list");
+
     pullDownButton.addEventListener('mouseover',function(){
         this.setAttribute('style',"background-color:blue;")
         console.log("乗せたときは青色")
     })
-})
 
-window.addEventListener('load',function(){
-    const pullDownButton = document.getElementById("lists");
     pullDownButton.addEventListener('mouseout',function(){
         this.removeAttribute('style',"background-color:red;")
         console.log("外れた")
     })
-})
 
-
-window.addEventListener('load',function(){
-const pullDownButton = document.getElementById("lists");
-const pullDownParents = document.getElementById("pull-down");
-pullDownButton.addEventListener('click',function(){
+    pullDownButton.addEventListener('click',function(){
     if (pullDownParents.getAttribute("style")=="display:block;"){
         pullDownParents.removeAttribute("style","display:block;")
         console.log("非表示")
@@ -29,4 +25,16 @@ pullDownButton.addEventListener('click',function(){
     console.log("表示")
     }
     })
+
+   pullDownChild.forEach(function(list){
+    list.addEventListener("click", function(){
+        const value = list.innerHTML
+        currentList.innerHTML = value ;
+        console.log(value);
+    })
 })
+    pullDownChildren.addEventListener('click',function(){
+         console.log(location.href="https://github.com/airikenmoto/task1.git") 
+   })
+   }
+window.addEventListener('load',pullDown)
